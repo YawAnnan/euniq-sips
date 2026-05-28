21d9ec928a61592e1037485c417258d8d0c19188
YawAnnan <annanamoahisaac585@outlook.com>
2026-05-28 16:51:23 +0000

    Updated the home page

--- data/products.ts
+++ data/products.ts
@@ -4,7 +4,7 @@ export const products = [
     name: "Guava",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4?q=80&w=1200&auto=format&fit=crop",
+      "/images/GUAVA.png",
   },
 
 {
@@ -12,48 +12,62 @@ export const products = [
     name: "Mango",
     price: 90,
     image:
-            "https://www.pittmandavis.com/images/xl/PD24-FloridaMangoes.webp?v=1",
+      "/images/MANGO.png",
   },
   {
     id: 3,
     name: "Pineapple",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?q=80&w=1200&auto=format&fit=crop",
+      "/images/PINEAPPLE.png",
   },
   {
     id: 4,
-    name: "Strawberry",
+    name: "Orange",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1464965911861-746a04b4bca6?q=80&w=1200&auto=format&fit=crop",
+      "/images/ORANGE.png",
   },
     {
     id: 5,
-    name: "Banana",
+    name: "Banana Milk Mix",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1603833665858-e61d17a86224?q=80&w=1200&auto=format&fit=crop",
+      "/images/BANANA.png",
   },
   {
     id: 6,
     name: "Grapes",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1537640538966-79f369143f8f?q=80&w=1200&auto=format&fit=crop",
+      "/images/GRAPES.png",
   },
   {
     id: 7,
     name: "Chocolate",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1606313564200-e75d5e30476c?q=80&w=1200&auto=format&fit=crop",
+      "/images/CHOCOLATE.png",
   },
   {
     id: 8,
     name: "Cocktail",
     price: 90,
     image:
-      "https://images.unsplash.com/photo-1551024709-8f23befc6f87?q=80&w=1200&auto=format&fit=crop",
+      "/images/COCKTAIL.png",
+  },
+  {
+    id: 9,
+    name: "Strawberry Milk Mix",
+    price: 90,
+    image:
+      "/images/STRAWBERRY.png",
+  },
+    {
+    id: 10,
+    name: "Fruit Mix",
+    price: 90,
+    image:
+      "/images/FRUIT.png",
   },
 ];

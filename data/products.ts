bcd78acd422db8357f9555182a3fa430bd412f9b
YawAnnan <annanamoahisaac585@outlook.com>
2026-05-28 14:14:57 +0000

    Add featured product images

--- data/products.ts
+++ data/products.ts
@@ -2,28 +2,56 @@ export const products = [
   {
     id: 1,
     name: "Guava",
-    price: 120,
+    price: 80,
     image:
       "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4?q=80&w=1200&auto=format&fit=crop",
   },
   {
     id: 2,
     name: "Mango",
-    price: 120,
+    price: 80,
     image:
       "https://images.unsplash.com/photo-1553279768-865429fa0078?q=80&w=1200&auto=format&fit=crop",
   },
   {
     id: 3,
     name: "Pineapple",
-    price: 120,
+    price: 80,
     image:
       "https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?q=80&w=1200&auto=format&fit=crop",
   },
   {
     id: 4,
     name: "Strawberry",
-    price: 120,
+    price: 80,
+    image:
+      "https://images.unsplash.com/photo-1464965911861-746a04b4bca6?q=80&w=1200&auto=format&fit=crop",
+  },
+    {
+    id: 5,
+    name: "Guava",
+    price: 80,
+    image:
+      "https://images.unsplash.com/photo-1623065422902-30a2d299bbe4?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 6,
+    name: "Mango",
+    price: 80,
+    image:
+      "https://images.unsplash.com/photo-1553279768-865429fa0078?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 7,
+    name: "Pineapple",
+    price: 80,
+    image:
+      "https://images.unsplash.com/photo-1589820296156-2454bb8a6ad1?q=80&w=1200&auto=format&fit=crop",
+  },
+  {
+    id: 8,
+    name: "Strawberry",
+    price: 80,
     image:
       "https://images.unsplash.com/photo-1464965911861-746a04b4bca6?q=80&w=1200&auto=format&fit=crop",
   },

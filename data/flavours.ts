53483be779111aa084404683f824c83de620455e
YawAnnan <annanamoahisaac585@outlook.com>
2026-06-13 18:26:40 +0000

    Added the flavour file to the project

---
+++ data/flavours.ts
@@ -0,0 +1,94 @@
+export const flavours = [
+  {
+    id: 1,
+    name: "Guava",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Rich tropical guava flavor crafted for a refreshing experience.",
+    color: "#556B2F",
+    image: "/images/guava.png",
+  },
+
+  {
+    id: 2,
+    name: "Mango",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Sweet mango goodness bursting with tropical freshness.",
+    color: "#D97706",
+    image: "/images/mango.png",
+  },
+
+  {
+    id: 3,
+    name: "Orange",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Bright citrus refreshment made for every occasion.",
+    color: "#F97316",
+    image: "/images/orange.png",
+  },
+  {
+    id: 4,
+    name: "Strawberry Milk Mix",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Sweet strawberry milk mix with a creamy texture and delightful flavor.",
+    color: "#F97316",
+    image: "/images/Strawberry.png",
+  },
+  {
+    id: 5,
+    name: "Banana Milk Mix",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Sweet banana milk mix with a creamy texture and delightful flavor.",
+    color: "#F97316",
+    image: "/images/Banana.png",
+  },
+  {
+    id: 6,
+    name: "Pineapple",
+    subtitle: "Natural Fruit Drink",
+    description:
+      " Tropical pineapple refreshment with a perfect balance of sweet and tangy flavors, crafted for a revitalizing experience.",
+    color: "#F97316",
+    image: "/images/Pineapple.png",
+  },
+  {
+    id: 7,
+    name: "Apple",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Crisp apple flavor with a perfect balance of sweet and tart notes, crafted for a refreshing experience.",
+    color: "#F97316",
+    image: "/images/Apple.png",
+  },
+  {
+    id: 8,
+    name: "Grapes",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Sweet grape flavor with a perfect balance of sweet and tart notes, crafted for a refreshing experience.",
+    color: "#F97316",
+    image: "/images/Grapes.png",
+  },
+ {
+    id: 9,
+    name: "chocolate Milk Mix",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "Rich chocolate flavor with a creamy texture and delightful taste.",
+    color: "#F97316",
+    image: "/images/Chocolate.png",
+  },
+ {
+    id: 10,
+    name: "Cocktail",
+    subtitle: "Natural Fruit Drink",
+    description:
+      "A delightful cocktail blend with a perfect balance of flavors.",
+    color: "#F97316",
+    image: "/images/Cocktail.png",
+  },
+];
